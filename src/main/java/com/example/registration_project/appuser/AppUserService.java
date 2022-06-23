c804cb5698fc5116039028a692c819dbafa59461
krios2146 <daun2146@yandex.ru>
2022-06-23 15:27:51 +0700

    Create AppUserRepository and AppUserService

---
+++ src/main/java/com/example/registration_project/appuser/AppUserService.java
@@ -0,0 +1,24 @@
+package com.example.registration_project.appuser;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserService implements UserDetailsService {
+
+    private final AppUserRepository repository;
+
+    @Autowired
+    public AppUserService(AppUserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return repository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email " + email + " not found"));
+    }
+}
