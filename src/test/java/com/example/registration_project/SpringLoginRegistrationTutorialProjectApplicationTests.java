9ae7364f502487fb710129abd74fe7cb1c0fa395
krios2146 <daun2146@yandex.ru>
2022-06-23 10:46:53 +0700

    Rename files

--- src/test/java/com/example/registration_project/SpringLoginRegistrationTutorialProjectApplicationTests.java
+++ src/test/java/com/example/registration_project/SpringLoginRegistrationTutorialProjectApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.springloginregistrationtutorialproject;
+package com.example.registration_project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
