4664a90ce7758a39f7c97175414599dd0d97e73b
krios2146 <daun2146@yandex.ru>
2022-06-23 10:44:09 +0700

    Initial commit

---
+++ src/main/java/com/example/springloginregistrationtutorialproject/SpringLoginRegistrationTutorialProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springloginregistrationtutorialproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoginRegistrationTutorialProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoginRegistrationTutorialProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springloginregistrationtutorialproject/SpringLoginRegistrationTutorialProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springloginregistrationtutorialproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginRegistrationTutorialProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
